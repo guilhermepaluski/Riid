b59f29eb4d0424ce9f2144c168518a7bee468296
Gustavo <gupaluski@gmail.com>
2025-06-02 20:05:07 -0300

    test: add initial tests

--- functions/CodeGenerator.cs
+++ functions/CodeGenerator.cs
@@ -9,19 +9,22 @@ namespace Riid.functions
     public class CodeGenerator
     {
         public static string GenerateSecureCode(int length = 8)
-    {
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        var bytes = new byte[length];
-        using var rng = RandomNumberGenerator.Create();
-        rng.GetBytes(bytes);
-
-        var result = new char[length];
-        for (int i = 0; i < length; i++)
         {
-            result[i] = chars[bytes[i] % chars.Length];
-        }
+            if (length <= 0)
+                return string.Empty;
 
-        return new string(result);
-    }
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var bytes = new byte[length];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(bytes);
+
+            var result = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = chars[bytes[i] % chars.Length];
+            }
+
+            return new string(result);
+        }
     }
 }
